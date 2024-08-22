58470df1fe221a7d8a58703692f11ef7e9fbe418
Hirun Senarathna <hirunsenarathna@users.noreply.github.com>
2024-08-23 00:33:52 +0530

    login php,register php modifyed

--- register.php
+++ register.php
@@ -15,7 +15,7 @@ if (isset($data->firstname, $data->lastname, $data->email, $data->username, $dat
     $lastname = $data->lastname;
     $email = $data->email;
     $username = $data->username;
-    $password = $data->password;
+    $password = password_hash($data->password, PASSWORD_DEFAULT); // Hash the password
     $city = $data->city;
     $telno = $data->telno;
     $role = "user";
@@ -28,12 +28,8 @@ if (isset($data->firstname, $data->lastname, $data->email, $data->username, $dat
         $account_id = $conn->insert_id; // Get the ID of the inserted row
 
         // Prepare and bind for the user table
-        $stmt_user = $conn->prepare("INSERT INTO customer (id, city, telno) VALUES (?, ?, ?)");
+        $stmt_user = $conn->prepare("INSERT INTO users (id, city, telno) VALUES (?, ?, ?)");
         $stmt_user->bind_param("iss", $account_id, $city, $telno);
-
-        // Set parameters and execute
-        $city = $data->city;
-        $telno = $data->telno;
 
         if ($stmt_user->execute() === TRUE) {
             echo json_encode(["message" => "Registration successful!"]);
@@ -52,3 +48,4 @@ if (isset($data->firstname, $data->lastname, $data->email, $data->username, $dat
 
 $stmt->close();
 $conn->close();
+?>
