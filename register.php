2d0299c2b5e2254fca9081c8731e7d750ec1fb11
MishelLiyanage <mailtomishel@gmail.com>
2024-08-29 08:32:34 +0530

    registration header updated

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // Include the database connection file
 require 'dbconnection.php';
