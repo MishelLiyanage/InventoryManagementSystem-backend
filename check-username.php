4dc88ff1dc5e889d3f2f722238a41fe74e4346e2
MishelLiyanage <mailtomishel@gmail.com>
2024-08-29 10:13:48 +0530

    backend headers updated

--- check-username.php
+++ check-username.php
@@ -2,7 +2,7 @@
 // Include the database connection file
 require 'dbconnection.php';
 
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
